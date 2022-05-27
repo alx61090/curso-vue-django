a0cc9c7cbefa3b458d4d91ab3194e42abae6b9be
alx61090 <alx61090@gmail.com>
2022-05-27 14:02:51 -0600

    inicial

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import router from './router'
+import axios from 'axios'
+
+
+const app = createApp(App)
+
+app.config.globalProperties.$axios = axios
+window.axios = axios
+
+app.use(router)
+
+app.mount('#app')
---
+++ src/pages/ListCategory.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Listado</h1>
+</template>
+
+
+<script>
+export default{
+  mounted(){
+    this.$axios.get('http://127.0.0.1:8000/api/category/all/?format=json').then((res)=>{
+      console.log(res.data)
+    })
+  }
+}
+</script>
